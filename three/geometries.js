04ed29c0a3d34ffad2cc8d332db04274b1ef1b2a
Edward <endalley11@gmail.com>
2017-03-23 17:14:21 +1300

    Developments to Three example , generating position

--- three/geometries.js
+++ three/geometries.js
@@ -5,6 +5,8 @@ var G = Jungle.G;
 var container
 var camera, scene, renderer;
 
+var SceneCell;
+
 init();
 animate();
 
@@ -25,7 +27,7 @@ function init() {
     map.anisotropy = 16;
 
     var material = new THREE.MeshLambertMaterial( { map: map, side: THREE.DoubleSide } );
-    //
+
     var shapeMaker = G({
         type:G(
             ["circle", "square"],{
@@ -54,9 +56,61 @@ function init() {
 
     });
 
+    var TimeVariant = G({
+            speed:5
+        },{
+        c(){
+            return Date.now() * 0.0001;
+        },
+        r(obj, arg, time){
+            return time*obj.speed;
+        }
+    })
 
-    scene.add(shapeMaker.resolve());
+    var circularTimeVariant = TimeVariant.X({
+        speed:5,
+        radius:50
+    },{
+        r({speed, radius}, arg, time){
+            return radius * Math.sin(time*speed);
+        }
+    });
 
+    var shapeCell = G({
+        rotation:{
+            x:TimeVariant.X({speed:0}),
+            y:TimeVariant
+        },
+        position:{
+            x:circularTimeVariant.X({radius:200, speed:20}),
+            y:circularTimeVariant,
+            z:0
+        }
+    },{
+        x:'shape use glscene',
+        p(){
+            this.shape = shapeMaker.resolve();
+            this.scene.add(this.shape)
+        },
+        r(obj){
+            this.shape.rotation.x = obj.rotation.x;
+            this.shape.rotation.y = obj.rotation.y;
+            this.shape.position.set(obj.position.x, obj.position.y, obj.position.z);
+        },
+        shape:undefined
+    });
+
+    SceneCell = G({
+        shape1:shapeCell
+    },{
+        x:'glscene',
+
+        p(scene){
+            this.scene = scene;
+        },
+
+        scene:undefined
+    }).prepare(scene);
 
     object = new THREE.AxisHelper( 50 );
     object.position.set( 200, 0, -200 );
@@ -92,10 +146,7 @@ function render() {
     camera.position.x = Math.cos( timer ) * 800;
     camera.position.z = Math.sin( timer ) * 800;
     camera.lookAt( scene.position );
-    for ( var i = 0, l = scene.children.length; i < l; i ++ ) {
-        var object = scene.children[ i ];
-        object.rotation.x = timer * 5;
-        object.rotation.y = timer * 2.5;
-    }
+
+    SceneCell.resolve(timer);
     renderer.render( scene, camera );
 }
